f6a75b461bd0225b9fce9e1ba2f23bd94c6514a4
윤선용 <sunyong9059@naver.com>
2023-06-20 23:28:05 +0900

    useContext 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Router from "shared/Router";
 
+export const AuthContext = createContext({
+  isLog: false,
+  setIsLog: () => {},
+})
+
 const App = () => {
-  return <Router />
+  const access_token = localStorage.getItem('access_token')
+  const [isLog, setIsLog] = useState(!!access_token)
+  console.log(isLog);
+
+  return (
+    <AuthContext.Provider value={{ isLog, setIsLog }}>
+      <Router />
+    </AuthContext.Provider>
+  )
 }
 
 export default App;
