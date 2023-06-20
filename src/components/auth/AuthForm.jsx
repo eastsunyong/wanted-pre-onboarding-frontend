8b46d8c892be7167ea0168ed8a6089f8b114b02a
윤선용 <sunyong9059@naver.com>
2023-06-20 23:29:07 +0900

    공용 컴포넌트 ui

---
+++ src/components/auth/AuthForm.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import SigninBtn from "./SigninBtn";
+import SignupBtn from "./SignupBtn";
+
+const AuthForm = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [massege, setMassege] = useState('');
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+
+  //이메일 유효성
+  const EmailCheck = (e) => {
+    const currentEmail = e.target.value;
+    setEmail(currentEmail);
+    const emailCheckReg = /.*@.*/;
+
+    if (!emailCheckReg.test(currentEmail)) {
+      setMassege("이메일을 확인해주세요");
+    } else {
+      setMassege('');
+      setEmailValid(true);
+    }
+  }
+    //비밀번호 유효성 유효성
+    const PasswordCheck = (e) => {
+      const currentPassword = e.target.value;
+      setPassword(currentPassword);
+      const passwordCheckReg = /^\S{8,}$/;
+  
+      if (!passwordCheckReg.test(currentPassword)) {
+        setMassege("8자 이상입니다");
+      } else {
+        setMassege('');
+        setPasswordValid(true);
+      }
+    }
+  return (
+    <>
+      <Sort>
+        <div>
+          <p>이메일</p>
+        </div>
+        <input
+          data-testid="email-input"
+          value={email}
+          placeholder="이메일을 입력해주세요"
+          onChange={EmailCheck}
+        />
+      </Sort>
+      <Sort>
+        <div>
+          <p>비밀번호</p>
+        </div>
+        <input
+          data-testid="password-input"
+          value={password}
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          onChange={PasswordCheck}
+        />
+      </Sort>
+      <Msg>
+        <label>{massege}</label>
+      </Msg>
+      {
+        props.title === '회원가입' ?  <SignupBtn emailValid={emailValid} passwordValid={passwordValid} email={email} password={password}/> :
+        <SigninBtn emailValid={emailValid} passwordValid={passwordValid} email={email} password={password}/>
+      }
+    </>
+  );
+};
+
+const Sort = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.6rem;
+  font-weight: 600;
+  margin-bottom: 2rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 8rem;
+    height: 3rem;
+    margin: 0 3rem 0 4rem;
+  }
+
+  input {
+    width: 30rem;
+    height: 3rem;
+    border-radius: 0.4rem;
+    border: 1px solid gray;
+    padding-left: 0.6rem;
+  }
+
+  .unable {
+  opacity: .5;
+  pointer-events: none;
+}
+`;
+
+const Msg = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: red;
+`
+
+const Button = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 4rem;
+    margin: 1rem 2rem;
+    border: none;
+    border-radius: 0.6rem;
+    background-color: #4ec5f4;
+    color: white;
+    font-size: 1.4rem;
+    font-weight: 600;
+`
+
+export default AuthForm;
---
+++ src/components/auth/Common.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import AuthForm from "./AuthForm";
+
+const Common = (props) => {
+  return (
+    <Wrap>
+      <Containar>
+        <h3>{props.title}</h3>
+         <AuthForm title={props.title}/>
+      </Containar>
+    </Wrap>
+  );
+};
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+const Containar = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50rem;
+  height: 28rem;
+  border-radius: 0.8rem;
+  border: 1px solid rgba(17, 24, 39, 0.25);
+  box-shadow: 0px 5px 8px rgba(17, 24, 39, 0.25);
+
+  h3 {
+    display: flex;
+    justify-content: center;
+    font-size: 2.5rem;
+    font-weight: bold;
+    padding-top: 1rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+export default Common;
